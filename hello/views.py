0f18644b0b6dd52132dacf254406b7838d25fe81
ValeraVV <valeravv@gmail.com>
2021-10-31 16:01:52 +0500

    add redirect to another region if n found

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ import requests
 from dateutil.parser import parse
 from django.shortcuts import render
 from json.decoder import JSONDecodeError
-from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse, HttpResponsePermanentRedirect
 from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Greeting, Stuff
@@ -32,18 +32,25 @@ def unrz(request, unrz):
             s.save()
     except ObjectDoesNotExist:
         try:
-            r = requests.get("https://www.gosuslugi.ru/api/vaccine/v2/cert/verify//unrz/"+unrz);
-            print(r)
-            j = r.json() 
-            s = Stuff.fromJson(j)
-            #update hello_stuff set r = unrz / 10000000000000 where r is null;
-            #update hello_stuff set n = unrz % 10000000000000 where is null;
-            #s.qr_url = s.scanQR()
-            s.save()
-        except JSONDecodeError:
-            return HttpResponseNotFound('noJson')           
-        except KeyError:
-            return HttpResponseNotFound('empty')           
+            s = Stuff.objects.get( n = int(unrz) % 10000000000000)
+            if not s.qr_url:
+                s.qr_url = s.scanQR()
+                s.save()
+            return HttpResponsePermanentRedirect(str(s.unrz)[1:])
+        except ObjectDoesNotExist:
+            try:
+                r = requests.get("https://www.gosuslugi.ru/api/vaccine/v2/cert/verify//unrz/"+unrz);
+                print(r)
+                j = r.json() 
+                s = Stuff.fromJson(j)
+                #update hello_stuff set r = unrz / 10000000000000 where r is null;
+                #update hello_stuff set n = unrz % 10000000000000 where is null;
+                #s.qr_url = s.scanQR()
+                s.save()
+            except JSONDecodeError:
+                return HttpResponseNotFound('noJson')           
+            except KeyError:
+                return HttpResponseNotFound('empty')           
     return JsonResponse(s.getJson())
 
 
@@ -71,7 +78,7 @@ function unrz(r,u) {
     return (r + 100).toString().substring(1) + (u + 10000000000000).toString().substring(1)
 }
 
-ri = 0; u = 1287837;
+ri = 0; u = 1287955;
 
 regs = [77,78,24,66,61,02,16,72,74,52,63,05,24,26,54,42,59,]
 
